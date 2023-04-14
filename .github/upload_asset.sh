5ef245a12d12fd25d6eadcd0a4e10c5eae11c003
Sayali M <sayali@Sayalis-MacBook-Pro>
2023-04-14 13:14:46 -0500

    fix: temp logic to create an empty .zip file for all extensions that do not generate .zip files

--- .github/upload_asset.sh
+++ .github/upload_asset.sh
@@ -43,6 +43,12 @@ declare -a StringArray=(".jar" ".jar.asc" ".jar.md5" ".jar.sha1" \
  "-javadoc.jar" "-javadoc.jar.asc" "-javadoc.jar.md5" "-javadoc.jar.sha1" \
  "-sources.jar" "-sources.jar.asc" "-sources.jar.md5" "-sources.jar.sha1" )
 
+# temp logic to create an empty .zip file for all extensions that do not generate .zip files
+ZIP_FILENAME="$ASSET_DIR/$ASSET_NAME_PREFIX$VERSION.zip"
+if [[ ! -f $ZIP_FILENAME ]]; then
+  touch $ZIP_FILENAME
+fi 
+
 for val in "${StringArray[@]}"; do
     FILE=$ASSET_DIR/$ASSET_NAME_PREFIX$VERSION$val
     # Skip if zip files do not exist (some extensions do not generate examples in zip format)
@@ -51,12 +57,11 @@ for val in "${StringArray[@]}"; do
         exit 1
     fi
     SIZE=$(wc -c $FILE | awk '{print $1}')
-    if [[ $SIZE -eq 0 ]]; then
+    #  if the file size is 0 and FILE is not a zip file then exit
+    if [[ $SIZE -eq 0 && "$FILE" != $ZIP_FILENAME ]]; then
         echo "$FILE is empty."
         exit 1;
     fi
     MIME=$(file -b --mime-type $FILE)
     upload_asset $FILE $SIZE $MIME
 done
-
-

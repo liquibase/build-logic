c7c7c6fd55e7107900ce551cd39a61cbb17b37c2
jandroav <jandroav@icloud.com>
2023-04-17 08:35:23 +0200

    chore(upload_asset.sh): remove unnecessary check for zip files chore(create-release.yml): update sonar-push.yml version to v0.3.1 chore(extension-attach-artifact-release.yml): update build-logic version to v0.3.1 chore(os-extension-test.yml): update sonar-pull-request.yml version to v0.3.1 chore(pro-extension-test.yml): update sonar-pull-request.yml version to v0.3.1

--- .github/upload_asset.sh
+++ .github/upload_asset.sh
@@ -39,14 +39,12 @@ upload_asset() {
 
 declare -a StringArray=(".jar" ".jar.asc" ".jar.md5" ".jar.sha1" \
  ".pom" ".pom.asc" ".pom.md5" ".pom.sha1" \
- ".zip" \
  "-javadoc.jar" "-javadoc.jar.asc" "-javadoc.jar.md5" "-javadoc.jar.sha1" \
  "-sources.jar" "-sources.jar.asc" "-sources.jar.md5" "-sources.jar.sha1" )
 
 for val in "${StringArray[@]}"; do
     FILE=$ASSET_DIR/$ASSET_NAME_PREFIX$VERSION$val
-    # Skip if zip files do not exist (some extensions do not generate examples in zip format)
-    if [[ ! -f "$FILE" && "$FILE" != *".zip" ]]; then
+    if [[ ! -f "$FILE" ]]; then
         echo "$FILE does not exist."
         exit 1
     fi
@@ -58,5 +56,3 @@ for val in "${StringArray[@]}"; do
     MIME=$(file -b --mime-type $FILE)
     upload_asset $FILE $SIZE $MIME
 done
-
-

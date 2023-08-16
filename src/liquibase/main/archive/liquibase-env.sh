772044546de3752ef241b6bccf7f51bfa0338968
jandroav <jandroav@icloud.com>
2023-08-16 09:28:12 +0200

    fix(liquibase): update LIQUIBASE_HOME path in postinst and liquibase-env.sh scripts
    The LIQUIBASE_HOME path in the postinst and liquibase-env.sh scripts was updated to "/opt/liquibase" to reflect the correct installation directory. This change ensures that the LIQUIBASE_HOME environment variable is set correctly and that the liquibase executable is added to the PATH.

--- src/liquibase/main/archive/liquibase-env.sh
+++ src/liquibase/main/archive/liquibase-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Needed when restarting the terminal
-echo "export LIQUIBASE_HOME=$LIQUIBASE_HOME" >> ~/.profile
+echo "export LIQUIBASE_HOME=/opt/liquibase" >> ~/.profile
 echo "export PATH=\$PATH:\$LIQUIBASE_HOME" >> ~/.profile
 source ~/.profile
